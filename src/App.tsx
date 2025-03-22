cbef42162a686f895694f7a93490972663bd4720
sakthi <sakthiehwaran6@gmail.com>
2025-03-23 05:08:17 +0530

    Comited

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/projects" element={<Projects/>} />
+            <Route path="/profile" element={<Projects/>} />
           </Routes>
         </main>
       </div>
